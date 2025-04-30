62bff43a6a39b809eaac9c8819991309ad199694
kushalanayaka <kushalnayaka875@gmail.com>
2025-04-30 11:07:45 +0530

    first commit

---
+++ TNS_Java/src/corejava/tns/demo.java
@@ -0,0 +1,21 @@
+package corejava.tns;
+
+public class demo {
+	
+	public static void main(String args[]) {
+		
+		int num = 12;
+		float decimal = 23.099f;
+		long L = 12904904;
+		double D = 23.839047093;
+		byte B = 127;
+		short S = 2;
+		boolean bool = true;
+		char C = 'k';
+		
+		String name = "KUSHAL";
+		
+		System.out.println(name);
+	}
+
+}
