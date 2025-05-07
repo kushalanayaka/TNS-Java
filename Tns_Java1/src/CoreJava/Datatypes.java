181c781bd7ccf58a9345cf6796972269644be6fe
Kushal <Kushal@DESKTOP-EQUFKJ1.mshome.net>
2025-05-07 14:36:00 +0530

    second commit

---
+++ Tns_Java1/src/CoreJava/Datatypes.java
@@ -0,0 +1,18 @@
+package CoreJava;
+
+public class Datatypes {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int num = 12;
+		String str = "kushal";
+		boolean bool = true;
+		
+		System.out.println("Number: "+num);
+		System.out.println("Name: "+str);
+		System.out.println("Boy: "+bool);
+
+	}
+
+}
