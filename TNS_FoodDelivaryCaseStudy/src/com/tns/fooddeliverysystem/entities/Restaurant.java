677a34f1c57e3a30cefdd848877ed9ad903d3bb3
kushalanayaka <kushalnayaka875@gmail.com>
2025-05-15 12:53:43 +0530

    Third Commit

---
+++ TNS_FoodDelivaryCaseStudy/src/com/tns/fooddeliverysystem/entities/Restaurant.java
@@ -0,0 +1,46 @@
+package com.tns.fooddeliverysystem.entities;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+
+public class Restaurant {
+    private int id;
+    private String name;
+    private List<FoodItem> menu;
+
+    public Restaurant(int id, String name) {
+        this.id = id;
+        this.name = name;
+        this.menu = new ArrayList<>();
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public List<FoodItem> getMenu() {
+        return menu;
+    }
+
+    public void addFoodItem(FoodItem foodItem) {
+        menu.add(foodItem);
+    }
+
+  
+
+    @Override
+    public String toString() {
+        return "Restaurant{id=" + id + ", name='" + name + "', menu=" + menu + '}';
+    }
+
+	public void removeFoodItem(int foodItemId) {
+		menu.removeIf(item -> item.getId() == foodItemId);		
+	}
+
+	
+}
